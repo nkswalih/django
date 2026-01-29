975fbae454478ec59aea9bfb73bcdf45bc3b9893
Mohammed Swalih N K <swalink555@gmail.com>
2026-01-29 17:10:34 +0530

    Authentication Page

---
+++ authproject/authentication/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+# Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
